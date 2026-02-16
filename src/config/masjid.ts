9f1d91cc130e1c9e2402836247402ca98248617a
Muhammad Asjad Rehman Hashmi <MuhammadAsjad.RehmanHashmi@gmail.com>
2026-02-16 12:19:07 -0600

    Fix madhab typing

--- src/config/masjid.ts
+++ src/config/masjid.ts
@@ -3,10 +3,15 @@ export const masjid = {
   timezone: "America/Chicago",
   coordinates: { lat: 31.3271, lon: -89.2903 },
 
+  export type MadhabType = "SHAFI" | "HANAFI";
+
+export const masjid = {
+  name: "Islamic Center of Hattiesburg",
   calc: {
-    method: "MOON_SIGHTING_COMMITTEE" as const,
+    method: "NorthAmerica",
     fajrAngle: 18,
     ishaAngle: 18,
-    madhab: "SHAFI" as const,
+    madhab: "HANAFI" as MadhabType,
   },
+}
 };
